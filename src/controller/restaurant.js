32c135171285220a5b8e476de88b59dec9ec41cb
Francisco Arredondo <frarredo@gmail.com>
2019-07-17 20:42:10 -0700

    added retreiving restaurants

--- src/controller/restaurant.js
+++ src/controller/restaurant.js
@@ -5,7 +5,7 @@ import Restaurant from '../model/restaurant';
 export default ({ config, db }) => {
   let api = Router();
 
-  // '/v1/restaurant/add'
+  // '/v1/restaurant/add' - Create
   api.post('/add', (req, res) => {
     let newRest = new Restaurant();
     newRest.name = req.body.name;
@@ -17,5 +17,26 @@ export default ({ config, db }) => {
       res.json({ message: 'restaurant saved sucessfully' });
     });
   });
+
+  // '/v1/restaurant' - Read
+  api.get('/', (req, res) => {
+    Restaurant.find({}, (err, restaurants) => {
+      if (err) {
+        res.send(err);
+      }
+      res.json(restaurants);
+    });
+  });
+
+  // '/v1/restaurant/:id'
+  api.get('/:id', (req, res) => {
+    Restaurant.findById(req.params.id, (err, restaurant) => {
+      if (err) {
+        res.send(err);
+      }
+      res.json(restaurant);
+    });
+  });
+
   return api;
 };
